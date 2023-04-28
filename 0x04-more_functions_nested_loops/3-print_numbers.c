57787fb810a1f0062f051dd99b0745304f95d316
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-04-28 07:43:10 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_numbers - print 0 - 9
  * Return: void
@@ -8,7 +9,7 @@ void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c = '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar (c);
 	}
