3a91b01224edc4882fba7c899733fa963486b98a
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-05-04 10:32:46 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,18 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
