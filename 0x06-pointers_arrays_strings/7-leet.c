ef7196b0417af14769af44bd42dc4e264986602f
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-05-04 12:24:52 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,14 +3,14 @@
 /**
  * leet - Encodes a string into 1337
  * @n: input
- * Return: The value of n
+ * Return: the value of n
  */
 char *leet(char *n)
 {
 	int i, j;
 
 	char s1[] = "aAeEoOtTlL";
-	char s2[] = 4433007711;
+	char s2[] = "4433007711";
 
 	for (i = 0; n[1] != '\0'; i++)
 	{
