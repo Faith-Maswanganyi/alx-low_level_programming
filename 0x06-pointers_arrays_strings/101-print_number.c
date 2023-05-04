8cc02874a31a08e5040a591b54d5d554a8b89fe0
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-05-04 14:12:39 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number -print number putchar
- * @n:integer
+ * print_number - print number putchar
+ * @n: integer
  * Return: void
  */
 void print_number(int n)
