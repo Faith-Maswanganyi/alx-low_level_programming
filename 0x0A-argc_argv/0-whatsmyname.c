86d502a2564f1b1fafd0f5b0e94ee4ba62cf9b4a
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-05-12 06:23:50 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * main - prints the program name
+ * main - prints the name of the program
  * @argc: Number arguments
  * @argv: Array name
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv);
+	printf("%s\n", argv[argc * 0]);
 	return (0);
 }
