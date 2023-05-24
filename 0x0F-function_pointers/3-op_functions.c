f78f99feb5d615bec7b1c2eef055d0a6b2127da6
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-05-24 12:26:22 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,11 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * op_add - adds a and b
- * @a: first number
- * @b: second number
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: sum of a and b
+ * Return: add.
  */
 int op_add(int a, int b)
 {
@@ -15,11 +13,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - subtracts b from a
- * @a: first number
- * @b: second number
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: difference of a and b
+ * Return: difference.
  */
 int op_sub(int a, int b)
 {
@@ -27,11 +25,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiplies a and b
- * @a: first number
- * @b: second number
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: multiplication of a and b
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
 {
@@ -39,11 +37,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - divides a by b
- * @a: first number
- * @b: second number
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: integer division of a by b
+ * Return: division.
  */
 int op_div(int a, int b)
 {
@@ -56,11 +54,11 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - find the remainder of the division of a by b
- * @a: first number
- * @b: second number
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: remainder of the division of a by b
+ * Return: remainder of the division.
  */
 int op_mod(int a, int b)
 {
