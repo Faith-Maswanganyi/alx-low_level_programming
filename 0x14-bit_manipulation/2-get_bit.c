1dd0dd7cd76716998a37142ff0090f3d434f6fc2
Faith-Maswanganyi <fm.maswanganyi@gmail.com>
2023-06-08 10:16:31 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value;
-
 	if (index > 63)
 		return (-1);
-
-	bit_value = (n >> index) & 1;
-
-	return (bit_value);
+	if (index == 0)
+	{
+		return (n & 1);
+	}
+	return (get_bit(n >> 1, index - 1));
 }
